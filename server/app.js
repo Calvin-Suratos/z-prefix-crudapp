191bd2bdf4833a0552f82ec4f69cc808daa4e32d
calvin-suratos <calvins94@JUDY-XPS.localdomain>
2022-06-29 11:50:09 -0400

    implemented back-end api server. retrieved data from database to localhost:8080

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV||'development']);
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json()); 
+
+
+app.get('/', (req, res) => res.status(200).send('Hello World!'))
+
+app.get('/users', (req, res) => {
+  knex 
+    .select('*')
+    .from('users')
+    .then(data => res.status(200).json(data))
+    .catch(() => res.status(404).send('Could not retrieve data'))
+})
+
+app.get('/posts', (req, res) => {
+  knex 
+    .select('*')
+    .from('posts')
+    .then(data => res.status(200).json(data))
+    .catch(() => res.status(404).send('Could not retrieve data'))
+})
+module.exports = app;
